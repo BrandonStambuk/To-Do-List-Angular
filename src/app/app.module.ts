b7b30b15a1d33d047032851ef92d3231c259bbdb
BrandonStambuk <115127679+BrandonStambuk@users.noreply.github.com>
2023-07-23 15:51:31 -0400

    new task button added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
-
+import { HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
-
-  constructor() { }
+  serviceUrl: string;
+  constructor(private http: HttpClient ) { 
+      this.serviceUrl= "http://localhost:3000/tasks"
+  }
 }
