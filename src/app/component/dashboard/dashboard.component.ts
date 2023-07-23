0322a9dc6028195d884bacaed2477f5c41a7ef83
BrandonStambuk <115127679+BrandonStambuk@users.noreply.github.com>
2023-07-23 16:56:56 -0400

    add task function added

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Task } from 'src/app/model/task';
+import { CrudService } from 'src/app/service/crud.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+    taskObj: Task = new Task();
+    taskArr: Task[]=[];
+    addTaskValue: string ='';
 
+    constructor(private crudService:CrudService){
+    
+    }
+
+    ngOnInit():void{
+      this.taskObj = new Task();
+      this.getAllTask();
+    }
+    
+
+    getAllTask(){
+        this.crudService.getAllTask().subscribe(res=>{
+          this.taskArr = res;
+        },err =>{
+          alert("Error a la hora de obtener las tareas");
+        }); 
+      
+    }
+    addTask(){
+      this.taskObj.task_name = this.addTaskValue;
+      this.crudService.addTask(this.taskObj).subscribe(res => {
+        this.ngOnInit();  // el metodo llama al metodo del servicio CRUD que recibe como parametro
+                          //una tarea si no hay un error se actualiza el componente EL ACTUAL
+      },err=> {
+        alert(err);
+      })
+    }
+
+    editTask(){
+      this.crudService.editTask(this.taskObj).subscribe(res => {
+        this.ngOnInit();  
+      },err=> {
+        alert("Fallo al editar la tarea");
+      })
+    }
+
+    deleteTask(etask: Task){
+      this.crudService.deleteTask(this.taskObj).subscribe(res => {
+        this.ngOnInit();  
+      },err=> {
+        alert("Fallo al eliminar la tarea");
+      })
+    }
 }
--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Task } from '../model/task';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,4 +10,19 @@ export class CrudService {
   constructor(private http: HttpClient ) { 
       this.serviceUrl= "http://localhost:3000/tasks"
   }
+
+
+  addTask(task: Task):Observable<Task>{
+    return this.http.post<Task>(this.serviceUrl,task);
+  }
+  getAllTask():Observable<Task[]>{
+    return this.http.get<Task[]>(this.serviceUrl);
+  }
+  deleteTask(task: Task):Observable<Task>{
+    return this.http.delete<Task>(this.serviceUrl+'/'+task);
+  }
+  editTask(task: Task):Observable<Task>{
+    return this.http.put<Task>(this.serviceUrl+'/'+task.id,task);
+  }
+ 
 }
